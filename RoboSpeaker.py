57ef28934d768bcf1ec511594bc9610b778d28f8
kartik882 <kartik89909@gmail.com>
2025-05-28 13:29:47 +0530

    Update RoboSpeaker.py

--- RoboSpeaker.py
+++ RoboSpeaker.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
             speech = input("Please type the speech you would like to say: ")
             if speech == "exit":
                 break
+                """Using Powershell command to say whatever is entered by user in speech"""
+                
             os.system(f"powershell -Command \"Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{speech}')\"")
         except Exception as e:
             print(e)
